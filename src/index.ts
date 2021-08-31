f6794d11000cdbcc1838e98141a62cfae241ae3c
Maria Reigan <maria@equestria-tcc.ru>
2021-08-31 15:02:16 +0700

    Multiple splashes

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,30 @@ import { elt } from "./util/elt";
 import { Game } from "./engine/Game";
 import { SplashScreenStage } from "./stages/SplashScreenStage";
 
+function loadImage(src: string): Promise<HTMLImageElement> {
+    return new Promise((resolve, reject) => {
+        elt.image(src, (image) => {
+            resolve(image);
+        }).onerror = reject;
+    });
+}
+
 function main() {
     const canvas = elt.canvas(400, 300);
     document.body.appendChild(canvas);
 
     const game = new Game(canvas);
-    const image = elt.image("img/test-splash.png", (image) => {
-        const splash = new SplashScreenStage(image, () => {
-            console.log("splash finished");
+    Promise.all([
+        loadImage("img/test-splash.png"),
+        loadImage("img/test-splash-2.png"),
+    ]).then(([image1, image2]) => {
+        const splash = new SplashScreenStage(image1, () => {
+            const otherSplash = new SplashScreenStage(image2, () => {
+                game.popFrontStage();
+            });
+
+            game.popFrontStage();
+            game.pushStage(otherSplash);
         });
 
         game.pushStage(splash);
