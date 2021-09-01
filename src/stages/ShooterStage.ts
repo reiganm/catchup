e04946ae1716dddc3c65a151475d6c2f7096ba81
Maria Reigan <maria@equestria-tcc.ru>
2021-09-01 15:48:13 +0700

    Basic shooting

--- src/stages/ShooterStage.ts
+++ src/stages/ShooterStage.ts
@@ -7,12 +7,19 @@ type ShooterStageConfig = {
     background: HTMLImageElement
 };
 
+/** Object that ShooterObjects can request to spawn other objects like bullets */
+interface ObjectSpawner {
+    spawn(object: ShooterObject)
+    despawn(object: ShooterObject)
+}
+
 /** Generic class for shooter stage objects: ships, bullets, powerups, etc. */
 class ShooterObject {
     x: number;
     y: number;
     velocity: Vector;
     bbox: BBox;
+    spawner: ObjectSpawner;
 
     constructor(x: number, y: number, bbox: BBox) {
         this.x = x;
@@ -21,8 +28,13 @@ class ShooterObject {
         this.bbox = bbox;
     }
 
+    /** BBox of the object but in world coordinates */
+    get effectiveBBox(): BBox {
+        return this.bbox.translated(this.x, this.y);
+    }
+
     render(context: CanvasRenderingContext2D) {
-        const box = this.bbox.translated(this.x, this.y);
+        const box = this.effectiveBBox;
         context.fillStyle = "white";
         context.fillRect(
             Math.floor(box.minX),
@@ -53,6 +65,9 @@ function oppositeDirection(direction: ControlDirection): ControlDirection {
 class PlayerShip extends ShooterObject {
     activeDirections: Set<ControlDirection>;
     maximumSpeed: number;
+    isShooting: boolean;
+    shootingTimer: number;
+    shootingTimerResetValue: number;
 
     constructor() {
         super(20, 150, new BBox(
@@ -61,6 +76,9 @@ class PlayerShip extends ShooterObject {
 
         this.maximumSpeed = 250;
         this.activeDirections = new Set();
+        this.isShooting = false;
+        this.shootingTimer = 0;
+        this.shootingTimerResetValue = 250;
     }
 
     get directionSymbol(): string {
@@ -76,7 +94,25 @@ class PlayerShip extends ShooterObject {
         this.activeDirections.delete(direction);
     }
 
+    startShooting() {
+        this.isShooting = true;
+    }
+
+    stopShooting() {
+        this.isShooting = false;
+    }
+
     update(dt: number) {
+        this.shootingTimer = Math.max(this.shootingTimer - dt, 0);
+        if (this.isShooting && this.shootingTimer === 0 ) {
+            this.shootingTimer = this.shootingTimerResetValue;
+            const box = this.effectiveBBox;
+            this.spawner.spawn(new Bullet(
+                box.minX + box.width,
+                box.minY + box.height / 2
+            ));
+        }
+
         switch (this.directionSymbol) {
             case "w":
                 this.velocity = new Vector(0, -1);
@@ -116,24 +152,65 @@ class PlayerShip extends ShooterObject {
     }
 }
 
+class Bullet extends ShooterObject {
+    constructor(x: number, y: number) {
+        super(x, y, new BBox(
+            0, -2, 8, 4
+        ));
+
+        this.velocity = new Vector(500, 0);
+    }
+}
+
 export class ShooterStage extends Stage {
     player: PlayerShip;
+    objects: ShooterObject[];
 
     constructor(config: ShooterStageConfig) {
         super();
         
         this.player = new PlayerShip();
         this.player.velocity = new Vector(20, 40);
+        this.objects = [];
+
+        this.addObject(this.player);
+    }
+
+    private addObject(object: ShooterObject) {
+        this.objects.push(object);
+        const stage = this;
+        object.spawner = {
+            spawn(object: ShooterObject) {
+                stage.addObject(object);
+            },
+            despawn(object: ShooterObject) {
+                stage.removeObject(object);
+            }
+        };
+    }
+
+    private removeObject(object: ShooterObject) {
+        const index = this.objects.findIndex(x => x === object);
+        if (index === -1) {
+            console.warn("attempting to remove non-spawned object:", object);
+            return;
+        }
+        const [removedObject] = this.objects.splice(index);
+        removedObject.spawner = null;
     }
 
     update(dt: number) {
-        this.player.update(dt);
+        for (const object of this.objects) {
+            object.update(dt);
+        }
     }
 
     render(context: CanvasRenderingContext2D) {
         context.fillStyle = "black";
         context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-        this.player.render(context);
+        for (const object of this.objects) {
+            object.render(context);
+        }
 
         context.fillStyle = "white";
         context.font = "8px amiga4ever"
@@ -176,6 +253,13 @@ export class ShooterStage extends Stage {
                     this.player.addDirection("d");
                 } else if (event.state === "up") {
                     this.player.subtractDirection("d");
+                }
+                break;
+            case " ":
+                if (event.state === "down") {
+                    this.player.startShooting();
+                } else if (event.state === "up") {
+                    this.player.stopShooting();
                 }
                 break;
         }
